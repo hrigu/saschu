4aaa60e621a1dc0458332bd553e58f7ba2dc22a8
hrigu <chmuehlethaler@gmail.com>
2012-09-23 11:41:39 +0200

    validate: Passwortlänge angepasst

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable #password_length: See config/initializers/devisec.rb
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
