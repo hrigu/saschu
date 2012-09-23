a3fcdcc6cb4ac02cc1134040a421279cb4690527
hrigu <chmuehlethaler@gmail.com>
2012-09-23 20:22:32 +0200

    Rollen eingeführt

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,21 @@ users = [
     last_name: 'Muehlethaler',
     email: 'chmuehlethaler@freesurf.ch',
     password: 'please',
-    #user.add_role :admin
+    role: "admin"
+},
+{
+    first_name: 'Maria',
+    last_name: 'Albisetti',
+    email: 'maria.albisetti@gmail.com',
+    password: 'please',
+    role: "parent"
 },
 {
     first_name: 'Benedikt',
     last_name: 'Muehlethaler',
     email: 'bene@gmail.com',
     password: 'please',
+    role: "student"
 }
 
 ]
@@ -19,7 +27,10 @@ users = [
 #puts 'New user created: ' << user.name
 
 users.each do |u|
+  role = u.delete(:role)
   u[:password_confirmation] = u[:password]
   user = User.create! u
+  user.add_role role
+  user.save
   puts 'New user created: ' << user.first_name
 end
