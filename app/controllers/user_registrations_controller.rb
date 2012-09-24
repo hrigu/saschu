7a74af2f7c3163cb75a907d126d7fb3e4f1f68c1
hrigu <chmuehlethaler@gmail.com>
2012-09-24 14:51:00 +0200

    Sign up fuer parent und student funktioniert

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -5,27 +5,27 @@ class UserRegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    rolable = params[:user].delete(:rolable)
     #TODO remove role from form
-    params[:user].delete(:role)
+    rolable_type = params[:user].delete(:role)
+    rolable = params[:user].delete(:rolable)
 
     super
 
-    rolable_type = rolable.delete(:rolable_type)
+#    rolable_type = rolable.delete(:rolable_type)
     school_class_id = rolable.delete(:school_class)
 
     #TODO wie kann man direkt die Schulklasse assignen?
     child_class = rolable_type.camelize.constantize
-    resource.rolable = child_class.new(rolable)
-
-    resource.rolable.school_class = SchoolClass.find school_class_id
-
+    if (rolable_type == "Student")
+      resource.rolable = child_class.new(rolable)
+      resource.rolable.school_class = SchoolClass.find school_class_id
+    end
+    resource.rolable = child_class.new()
 
     valid = resource.valid?
     valid = resource.rolable.valid? && valid
 
     #resource.add_role role
-
 
 
     resource.save
